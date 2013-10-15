d5a1e0d39a564383b12e4bc26a95277fc4675e44
Phil Chrapka <pchrapka@gmail.com>
2013-10-15 13:41:18 -0400

    adding brain analysis

--- analysis/analysis_plot_brain_surface.m
+++ analysis/analysis_plot_brain_surface.m
@@ -12,7 +12,7 @@ aet_init
 cfg.file_in = ['..' filesep 'output' filesep ...
     'out_sim_vars_3_lcmv.mat'];
 cfg.iteration = 1;
-cfg.snr = -5;
+cfg.snr = 10;
 cfg.epsilon = ones(3,1)*sqrt(80^2/3);
 cfg = analysis_param_sweep_select_data(cfg);
 
