f0105fb4e6c019450b019de26612e36c0d915e1d
Phil Chrapka <pchrapka@gmail.com>
2013-10-16 11:49:33 -0400

    added plots for sims with multiple epsilons

--- analysis/analysis_simulation_ex2.m
+++ analysis/analysis_simulation_ex2.m
@@ -18,16 +18,15 @@ set(0,'DefaultAxesColorOrder',[0,0,0]) % black and white
 %% mult_cort_src
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% cfg.sim_name = 'sim_data_1';
-% cfg.source_name = 'mult_cort_src';
+cfg.sim_name = 'sim_data_1';
+cfg.source_name = 'mult_cort_src';
+cfg.exp_name = '';
+
 % cfg.exp_name = 'ex2_snr';
 % cfg.file_in = ['..' filesep 'output' filesep...
 %     'sim_data_1_mult_cort_src_ex2_snr.mat'];
 % analysis_plot_data(cfg);
-
-% cfg.sim_name = 'sim_data_1';
-% cfg.source_name = 'mult_cort_src';
-% cfg.exp_name = 'ex2_epsilon';
+%
 % cfg.file_in = ['..' filesep 'output' filesep...
 %     'sim_data_1_mult_cort_src_ex2_epsilon.mat'];
 % analysis_plot_data(cfg);
@@ -38,12 +37,17 @@ set(0,'DefaultAxesColorOrder',[0,0,0]) % black and white
 % cfg.file_in = ['..' filesep 'output' filesep...
 %     'sim_data_1_mult_cort_src_sine_ex2_snr.mat'];
 % analysis_plot_data(cfg);
+%
+cfg.file_in = ['..' filesep 'output' filesep...
+    'sim_data_1_mult_cort_src_ex2_snr_eps_many.mat'];
+analysis_plot_data(cfg);
 
 %% mult_cort_src_2
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% cfg.sim_name = 'sim_data_1';
-% cfg.source_name = 'mult_cort_src_2';
+cfg.sim_name = 'sim_data_1';
+cfg.source_name = 'mult_cort_src_2';
+
 % cfg.exp_name = '';
 % cfg.file_in = ['..' filesep 'output' filesep...
 %     'sim_data_1_mult_cort_src_2_ex2_snr_eps_10.mat'];
@@ -69,33 +73,41 @@ set(0,'DefaultAxesColorOrder',[0,0,0]) % black and white
 %     'sim_data_1_mult_cort_src_2_ex2_epsilon_0_800_snr20.mat'];
 % analysis_plot_data(cfg);
 
+cfg.file_in = ['..' filesep 'output' filesep...
+    'sim_data_1_mult_cort_src_2_ex2_snr_eps_many.mat'];
+analysis_plot_data(cfg);
+
 %% mult_cort_src_3
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 cfg.sim_name = 'sim_data_1';
 cfg.source_name = 'mult_cort_src_3';
 cfg.exp_name = '';
-cfg.file_in = ['..' filesep 'output' filesep...
-    'sim_data_1_mult_cort_src_3_ex2_snr_eps_10.mat'];
-analysis_plot_data(cfg);
+
+% cfg.file_in = ['..' filesep 'output' filesep...
+%     'sim_data_1_mult_cort_src_3_ex2_snr_eps_10.mat'];
+% analysis_plot_data(cfg);
+% 
+% cfg.file_in = ['..' filesep 'output' filesep...
+%     'sim_data_1_mult_cort_src_3_ex2_snr_eps_200.mat'];
+% analysis_plot_data(cfg);
+% 
+% cfg.file_in = ['..' filesep 'output' filesep...
+%     'sim_data_1_mult_cort_src_3_ex2_snr_eps_400.mat'];
+% analysis_plot_data(cfg);
+% 
+% cfg.file_in = ['..' filesep 'output' filesep...
+%     'sim_data_1_mult_cort_src_3_ex2_epsilon_0_800_snr-10.mat'];
+% analysis_plot_data(cfg);
+% 
+% cfg.file_in = ['..' filesep 'output' filesep...
+%     'sim_data_1_mult_cort_src_3_ex2_epsilon_0_800_snr10.mat'];
+% analysis_plot_data(cfg);
+% 
+% cfg.file_in = ['..' filesep 'output' filesep...
+%     'sim_data_1_mult_cort_src_3_ex2_epsilon_0_800_snr20.mat'];
+% analysis_plot_data(cfg);
 
 cfg.file_in = ['..' filesep 'output' filesep...
-    'sim_data_1_mult_cort_src_3_ex2_snr_eps_200.mat'];
+    'sim_data_1_mult_cort_src_3_ex2_snr_eps_many.mat'];
 analysis_plot_data(cfg);
-
-cfg.file_in = ['..' filesep 'output' filesep...
-    'sim_data_1_mult_cort_src_3_ex2_snr_eps_400.mat'];
-analysis_plot_data(cfg);
-
-cfg.file_in = ['..' filesep 'output' filesep...
-    'sim_data_1_mult_cort_src_3_ex2_epsilon_0_800_snr-10.mat'];
-analysis_plot_data(cfg);
-
-cfg.file_in = ['..' filesep 'output' filesep...
-    'sim_data_1_mult_cort_src_3_ex2_epsilon_0_800_snr10.mat'];
-analysis_plot_data(cfg);
-
-cfg.file_in = ['..' filesep 'output' filesep...
-    'sim_data_1_mult_cort_src_3_ex2_epsilon_0_800_snr20.mat'];
-analysis_plot_data(cfg);
-
