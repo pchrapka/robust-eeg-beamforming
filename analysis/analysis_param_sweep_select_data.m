28b62779c34141d3cc8883a1da0c72b77b2e5b69
Phil Chrapka <pchrapka@gmail.com>
2013-10-17 10:22:45 -0400

    adding some fieldtrip code

--- analysis/analysis_param_sweep_select_data.m
+++ analysis/analysis_param_sweep_select_data.m
@@ -13,7 +13,7 @@ function [ cfg ] = analysis_param_sweep_select_data(cfg)
 %
 %   Output
 %   cfg     same struct as before, with the following fields updated
-%       eeg_data    EEG data used for the analysis
+%       out         the selected data from parameter sweep
 %
 %   See also SIMULATION_VARIATIONS
 
@@ -37,15 +37,6 @@ if isfield(out_temp(1), 'epsilon')
         cellfun(@(x)isequal(x, cfg.epsilon),...
         {out_temp.epsilon}));
 end
-
-% Load the EEG data
-% NOTE At this point we should have filtered down to a single data set
-data_file = ['..' filesep out_temp(1).data_file];
-% Replace slashes just in case
-data_file = strrep(data_file, '/', filesep);
-load(data_file);    % loads variable data
-cfg.eeg_data = data.avg_trials;
-clear data;
 
 cfg.out = out_temp;
 
--- analysis/analysis_plot_brain_surface.m
+++ analysis/analysis_plot_brain_surface.m
@@ -16,6 +16,15 @@ cfg.snr = 10;
 cfg.epsilon = ones(3,1)*sqrt(80^2/3);
 cfg = analysis_param_sweep_select_data(cfg);
 
+%% Load the EEG data
+% NOTE At this point we should have filtered down to a single data set
+data_file = ['..' filesep cfg.out(1).data_file];
+% Replace slashes just in case
+data_file = strrep(data_file, '/', filesep);
+load(data_file);    % loads variable data
+cfg.eeg_data = data.avg_trials;
+clear data;
+
 % Save to a temp file
 eeg_data = cfg.eeg_data;
 save('temp.mat','eeg_data');
