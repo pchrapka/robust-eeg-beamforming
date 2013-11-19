8ab94d8998c2266107170eb52d61869d6452d281
Phil Chrapka <pchrapka@gmail.com>
2013-11-19 10:01:57 -0500

    redid the winscp scripts

--- winscp_download.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" /console /script="C:\Users\Phil\My Projects\robust-eeg-beamforming\winscp_download.txt" /log="C:\Users\Phil\My Projects\robust-eeg-beamforming\output\log.txt" 
