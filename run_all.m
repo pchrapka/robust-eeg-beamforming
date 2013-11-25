f6a25e07ca335ecde346ca7242dd673d74c2524b
Phil Chrapka <pchrapka@gmail.com>
2013-11-25 16:01:56 -0500

    adding the simulations to the main run script

--- run_all.m
+++ run_all.m
@@ -4,3 +4,8 @@
 run_sim_vars_single
 run_sim_vars_mult
 run_sim_vars_distr
+
+% Run all BEM simulations
+run_sim_vars_single_bem
+% run_sim_vars_mult_bem % Not until I figure out the issue
+run_sim_vars_distr_bem
