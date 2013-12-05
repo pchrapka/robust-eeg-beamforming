9820e203d776fb2ca5ffe01191d16d1fb0f6b17b
Phil Chrapka <pchrapka@gmail.com>
2013-12-05 13:04:31 -0500

    setup simulations for paper

--- run_all.m
+++ run_all.m
@@ -1,11 +1,16 @@
 %% run_all
 
-% Run all simulations
-run_sim_vars_single
-run_sim_vars_mult
-run_sim_vars_distr
+%% Run all simulations
+% run_sim_vars_single
+% run_sim_vars_mult
+% run_sim_vars_distr
 
-% Run all BEM simulations
-run_sim_vars_single_bem
-% run_sim_vars_mult_bem % Not until I figure out the issue
-run_sim_vars_distr_bem
+%% Run all BEM simulations
+% run_sim_vars_single_bem
+% run_sim_vars_mult_bem
+% run_sim_vars_distr_bem
+
+%% Paper simulations
+run_sim_vars_single_bem_paper
+run_sim_vars_mult_bem_paper
+run_sim_vars_distr_bem_paper
