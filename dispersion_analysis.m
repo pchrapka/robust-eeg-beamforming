a560c3e817dc43887fb005af790a789981fe0da0
Phil Chrapka <pchrapka@gmail.com>
2014-04-22 15:44:46 -0400

    more renaming

--- dispersion_analysis.m
+++ dispersion_analysis.m
@@ -1,21 +1,30 @@
 %% analyze_dispersion
+clc;
 
 %% Common params
 snr = '0';
-mismatch = false;
+mismatch = true;
 
 %% Get the data file name
 cfg = [];
 cfg.sim_name = 'sim_data_bem_1_100t';
 cfg.source_name = 'distr_cort_src_2';
+% cfg.source_name = 'single_cort_src_1';
 cfg.snr = snr;
 cfg.iteration = '1';
 if mismatch
-    cfg.tag = 'dispersion';
-else
     cfg.tag = 'dispersion_3sphere';
+else
+    cfg.tag = 'dispersion';
 end
-data_file = db.save_setup(cfg_out);
+data_file = db.save_setup(cfg);
 
 %% Load data
 data_in = load(data_file);
+results = data_in.dispersion_data;
+
+%% Display the results
+% results.name = results.name';
+% results.value = results.value';
+M = [results.name; results.value];
+fprintf('%s %f\n',M{:});
