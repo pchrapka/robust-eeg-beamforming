401a96fc34fb90da13e48aca85dad52dbc8874e1
Phil Chrapka <pchrapka@gmail.com>
2014-05-08 14:03:37 -0400

    configured the mult and distr sim configs

--- run_sim_vars_distr_bem_paper_rms.m
+++ run_sim_vars_distr_bem_paper_rms.m
@@ -16,20 +16,22 @@ k = 1;
 % Simulate ERP data
 scripts(k).func = @simulation_data;
 cfg = struct(...
-    'sim_data',             'sim_data_bem_1_100t',...
+    'sim_data',             'sim_data_bem_100_100t',...
     'sim_src_parameters',   'src_param_distr_cortical_source_2');
 scripts(k).vars = {cfg};
 k = k+1;
 
 %% Parameter sweep
 force = false;
+% 0.452 comes from the time point selected for display in the paper
+time_idx = 250*0.452;
 
 % Data files
 cfg_data = [];
-cfg_data.data_name = 'sim_data_bem_1_100t';
+cfg_data.data_name = 'sim_data_bem_100_100t';
 cfg_data.source_name = 'distr_cort_src_2';
-cfg_data.iteration_range = 1;
-cfg_data.snr_range = -10:10:0;%-20:10:0;
+cfg_data.iteration_range = 1:100;
+cfg_data.snr_range = 0;
 
 %% ==== MATCHED LEADFIELD ====
 
@@ -38,12 +40,14 @@ cfg_simvars_setup = [];
 cfg_simvars_setup.id = 'sim_vars_distr_src_paper_matched';
 cfg_simvars_setup.data = cfg_data;
 cfg_simvars_setup.force = force;
+cfg_simvars_setup.time_idx = time_idx;
 cfg_simvars_setup.head.type = 'brainstorm';
 cfg_simvars_setup.head.file = 'head_Default1_bem_500V.mat';
 cfg_simvars = sim_vars.get_config(cfg_simvars_setup);
 cfg = struct(...
     'sim_vars',             cfg_simvars,...
-    'analysis_run_func',    @beamformer_analysis);
+    'analysis_run_func',    @beamformer_analysis,...
+    'debug',                false);
 scripts(k).vars = {cfg};
 k = k+1;
 
@@ -54,6 +58,7 @@ cfg_simvars_setup = [];
 cfg_simvars_setup.id = 'sim_vars_distr_src_paper_mismatched';
 cfg_simvars_setup.data = cfg_data;
 cfg_simvars_setup.force = force;
+cfg_simvars_setup.time_idx = time_idx;
 cfg_simvars_setup.tag = '3sphere';
 cfg_simvars_setup.head.current.type = 'brainstorm';
 cfg_simvars_setup.head.current.file = 'head_Default1_3sphere_500V.mat';
