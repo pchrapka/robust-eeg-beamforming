e13a3d9ffc01738aebae313163f62ba478692b13
Phil Chrapka <pchrapka@gmail.com>
2014-05-12 16:13:19 -0400

    adding notes for fixes

--- rms_error.m
+++ rms_error.m
@@ -1,4 +1,12 @@
 function [rms, rms_peak] = rms_error(bf_power, true_peak, poi)
+
+% FIXME
+% 1. Calculate the scaling factor over the support of the input signal
+% Sum(bf_power)/Sum(input power) = alpha
+% 2. Normalize the output power bf_power/alpha
+% 3. Calculate the RMSE, i.e. take the sqrt of the sum of squared
+% difference between the input and output
+
 % Select all points except the peak
 poi(true_peak) = false;
 
