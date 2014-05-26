c79bbacaf9ed9c5facabfcbae69f26bafac67d74
Phil Chrapka <pchrapka@gmail.com>
2014-05-26 14:50:14 -0400

    refactoring a bit moved csv generation code to a separate function fixed a few bugs added a for loop to do rms analysis, instead of playing around with comments

--- rms_analysis.m
+++ rms_analysis.m
@@ -7,72 +7,58 @@ mismatch = true;
 clustered_data = false; % only for mult_src
 
 %% Get the data file name
-cfg = [];
-cfg.sim_name = 'sim_data_bem_1_100t';
-% cfg.source_name = 'distr_cort_src_2';
-cfg.source_name = 'single_cort_src_1';
-% cfg.source_name = 'mult_cort_src_10';
-cfg.snr = snr;
-cfg.iteration = '1';
+cfg_template = [];
+cfg_template.sim_name = 'sim_data_bem_1_100t';
+cfg_template.source_name = '';
+cfg_template.snr = snr;
+cfg_template.iteration = '1';
 if mismatch
-    cfg.tag = 'rms_3sphere';
+    cfg_template.tag = 'rms_3sphere';
 else
-    cfg.tag = 'rms';
+    cfg_template.tag = 'rms';
 end
 if clustered_data
-    cfg.tag = [cfg.tag '_cluster'];
+    cfg_template.tag = [cfg_template.tag '_cluster'];
 end
-data_file = db.save_setup(cfg);
 
-%% Load data
-data_in = load(data_file);
-results = data_in.rms_data;
+%% Set up configs
+k = 1;
+cfgs = cfg_template;
+cfgs.source_name = 'single_cort_src_1';
+k = k+1;
+cfgs(k) = cfg_template;
+cfgs(k).source_name = 'mult_cort_src_10';
+k = k+1;
+cfgs(k) = cfg_template;
+cfgs(k).source_name = 'distr_cort_src_2';
 
-%% Summarize the data
-[M, col_labels] = rms.rms_summarize(results);
 
-%% Display the results
-fprintf('%s | %s | %s | %s | %s\n', col_labels{:});
-fprintf('%s %d %f %f %f\n',M{:});
-
-
-%% Output the results
-% Set csv file name
-cfg.ext = '.csv';
-file_out =  db.save_setup(cfg);
-
-% Open the file
-file_ID = fopen(file_out,'wt');
-
-% Output the header
-cols = length(col_labels);
-for col_idx = 1:cols
-    fprintf(file_ID, '%s', col_labels{col_idx});
-    if col_idx ~= cols
-        fprintf(file_ID, ',');
+%% Summarize the results
+for i=1:length(cfgs)
+    % Select the config
+    cfg = cfgs(i);
+    fprintf('\n%s %s\n', cfg.source_name, cfg.tag);
+    
+    % Get the data file name
+    data_file = db.save_setup(cfg);
+    
+    % Load data
+    data_in = load(data_file);
+    results = data_in.rms_data;
+    
+    % Summarize the data
+    [M, col_labels] = rms.rms_summarize(results);
+    
+    % Display the results
+    fprintf('%s | %s | %s | %s | %s\n', col_labels{:});
+    if isequal(cfg.source_name,'mult_cort_src_10')
+        fprintf('%s %d %f %f %f %f\n',M{:});
+    else
+        fprintf('%s %d %f %f %f\n',M{:});
     end
+    
+    % Output the results to a csv file
+    cfg.rms_col_labels = col_labels;
+    cfg.rms_out = M;
+    rms.rms_summarize_csv(cfg);
 end
-fprintf(file_ID, '\n');
-
-% Output the data
-data = M';
-[rows, cols] = size(data);
-for row_idx = 1:rows
-    for col_idx = 1:cols
-        % Output data depending on type
-        if col_idx == 1
-            fprintf(file_ID, '%s', data{row_idx, col_idx});
-        elseif col_idx == 2
-            fprintf(file_ID, '%d', data{row_idx, col_idx});
-        else
-            fprintf(file_ID, '%f', data{row_idx, col_idx});
-        end
-        % Add comma except at the end of the row
-        if col_idx ~= cols
-            fprintf(file_ID, ',');
-        end
-    end
-    % New line at the end of the row
-    fprintf(file_ID, '\n');
-end
-fclose(file_ID);
--- rms_analysis_iterations.m
+++ rms_analysis_iterations.m
@@ -6,92 +6,57 @@ snr = '0';
 mismatch = true;
 clustered_data = false; % only for mult_src
 
-%% Get the data file name
-cfg = [];
-cfg.sim_name = 'sim_data_bem_100_100t';
-% cfg.source_name = 'distr_cort_src_2';
-cfg.source_name = 'single_cort_src_1';
-% cfg.source_name = 'mult_cort_src_10';
-cfg.snr = snr;
-cfg.iteration = '1-100';
+cfg_template = [];
+cfg_template.sim_name = 'sim_data_bem_100_100t';
+cfg_template.source_name = '';
+cfg_template.snr = snr;
+cfg_template.iteration = '1-100';
 if mismatch
-    cfg.tag = 'rms_3sphere';
+    cfg_template.tag = 'rms_3sphere';
 else
-    cfg.tag = 'rms';
+    cfg_template.tag = 'rms';
 end
 if clustered_data
-    cfg.tag = [cfg.tag '_cluster'];
+    cfg_template.tag = [cfg_template.tag '_cluster'];
 end
-data_file = db.save_setup(cfg);
 
-%% Load data
-data_in = load(data_file);
-results = data_in.rms_data;
+%% Set up configs
+k = 1;
+cfgs = cfg_template;
+cfgs.source_name = 'single_cort_src_1';
+k = k+1;
+cfgs(k) = cfg_template;
+cfgs(k).source_name = 'mult_cort_src_10';
+k = k+1;
+cfgs(k) = cfg_template;
+cfgs(k).source_name = 'distr_cort_src_2';
 
-%% Summarize the data
-[M, col_labels] = rms.rms_summarize(results);
-
-%% Display the results
-% rmse_2_rms_input = 20*log10(...
-%     results.rmse./results.rms_input);
-% % Output the columns
-% col_labels = {'Beamformer', 'Peak Index', 'RMS Error',...
-%     'RMS Input', '20log(RMSE/RMS Input)'};
-% M = {};
-% for i=1:size(results.rmse,2)
-%     A = [results.name;...
-%         num2cell(results.true_peak_idx(:,i)');...
-%         num2cell(results.rmse(:,i)');...
-%         num2cell(results.rms_input(:,i)');...
-%         num2cell(rmse_2_rms_input(:,i)')];
-%     
-%     M = [M A];
-% end
-if length(col_labels) == 5
-    fprintf('%s | %s | %s | %s | %s\n', col_labels{:});
-    fprintf('%s %d %f %f %f\n',M{:});
-else
+%% Summarize the results
+for i=1:length(cfgs)
+    % Select the config
+    cfg = cfgs(i);
+    fprintf('\n%s %s\n', cfg.source_name, cfg.tag);
+    
+    % Get the data file name
+    data_file = db.save_setup(cfg);
+    
+    % Load data
+    data_in = load(data_file);
+    results = data_in.rms_data;
+    
+    % Summarize the data
+    [M, col_labels] = rms.rms_summarize(results);
+    
+    % Display the results
     fprintf('%s | %s | %s | %s | %s | %s | %s \n', col_labels{:});
-    fprintf('%s %d %f %f %f %f %f\n',M{:});
-end
-
-%% Output the results
-% Set csv file name
-cfg.ext = '.csv';
-file_out =  db.save_setup(cfg);
-
-% Open the file
-file_ID = fopen(file_out,'wt');
-
-% Output the header
-cols = length(col_labels);
-for col_idx = 1:cols
-    fprintf(file_ID, '%s', col_labels{col_idx});
-    if col_idx ~= cols
-        fprintf(file_ID, ',');
+    if isequal(cfg.source_name,'mult_cort_src_10')
+        fprintf('%s | %d | %f | %f | %f | %f | %f | %f \n', M{:});
+    else
+        fprintf('%s | %d | %f | %f | %f | %f | %f \n', M{:});
     end
+    
+    % Output the results to a csv file
+    cfg.rms_col_labels = col_labels;
+    cfg.rms_out = M;
+    rms.rms_summarize_csv(cfg);
 end
-fprintf(file_ID, '\n');
-
-% Output the data
-data = M';
-[rows, cols] = size(data);
-for row_idx = 1:rows
-    for col_idx = 1:cols
-        % Output data depending on type
-        if col_idx == 1
-            fprintf(file_ID, '%s', data{row_idx, col_idx});
-        elseif col_idx == 2
-            fprintf(file_ID, '%d', data{row_idx, col_idx});
-        else
-            fprintf(file_ID, '%f', data{row_idx, col_idx});
-        end
-        % Add comma except at the end of the row
-        if col_idx ~= cols
-            fprintf(file_ID, ',');
-        end
-    end
-    % New line at the end of the row
-    fprintf(file_ID, '\n');
-end
-fclose(file_ID);
