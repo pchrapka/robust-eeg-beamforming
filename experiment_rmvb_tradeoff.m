db6d972e31f1f41b10f99be353279ccff3a342d9
Phil Chrapka <pchrapka@gmail.com>
2014-07-17 18:36:31 -0400

    added more configs, to rmvb tradeoff experiment

--- experiment_rmvb_tradeoff.m
+++ experiment_rmvb_tradeoff.m
@@ -1,11 +1,21 @@
+close all;
+
 %% Get the data
 % Set up config to get the data file
 snr = '0';
 
 cfg = [];
 cfg.beam_cfgs = {...
+    ...Matched
+    'rmv_epsilon_20',...
+    'lcmv',...
+    'lcmv_eig_0',...
+    'lcmv_reg_eig',...
+    ...Mismatched
     'lcmv_3sphere',...
     'lcmv_eig_0_3sphere',...
+    'lcmv_reg_eig_3sphere',...
+    'rmv_epsilon_150_3sphere',...
     'rmv_aniso_3sphere'};
 cfg.sim_name = 'sim_data_bem_1_100t';
 cfg.source_name = 'single_cort_src_1';
@@ -34,9 +44,9 @@ clear sim_cfg;
 eval(cfg.sim_name);
 eval(cfg.source_config);
 
-%% Figure out the dipole orientation
+%% Plot source components
 % Choose a source index to plot
-idx = 295;
+loc_idx = 295;
 component_labels = {'x','y','z'};
 for i=1:length(bf_data)
     figure;
@@ -44,7 +54,7 @@ for i=1:length(bf_data)
     for j=1:n_comp
         % Plot each component separately
         subplot(n_comp,1,j);
-        data = squeeze(bf_data{i}.bf_out(j,idx,:));
+        data = squeeze(bf_data{i}.bf_out(j,loc_idx,:));
         plot(data);
         if j==1
             % Add the title to the first plot
@@ -54,3 +64,19 @@ for i=1:length(bf_data)
         ylabel([component_labels{j} ' comp']);
     end
 end
+
+%% Calculate dipoles
+sample_idx = 120;
+loc_idx = 295;
+dipole_actual = sim_cfg.sources{1}.moment;
+fprintf('\n-- Dipoles --\n');
+for i=1:length(bf_data)
+    dipole = bf_data{i}.bf_out(:,loc_idx,sample_idx);
+    fprintf('%s\n',strrep(bf_data{i}.name,'_',' '));
+    dipole_mom = dipole/norm(dipole);
+    fprintf('[%f %f %f]\n',dipole_mom);
+    fprintf('error: %f\n',norm(dipole_mom - dipole_actual));
+end
+
+fprintf('Actual dipole moment\n');
+fprintf('[%f %f %f]\n', dipole_actual);
