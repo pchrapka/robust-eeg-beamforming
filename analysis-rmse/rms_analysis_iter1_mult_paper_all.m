d3e51ce61bf8757784479236f9011bab8ac06036
Phil Chrapka <pchrapka@gmail.com>
2014-08-06 11:28:20 -0400

    added lcmv eig 0 to rms analysis adding csv files to sync script

--- analysis-rmse/rms_analysis_iter1_mult_paper_all.m
+++ analysis-rmse/rms_analysis_iter1_mult_paper_all.m
@@ -6,6 +6,9 @@ util.update_aet();
 %% Initialize the Advanced EEG Toolbox
 aet_init
 
+cfg_par = [];
+aet_parallel_init(cfg_par);
+
 %% Set up different rms analysis 
 % Mult source results with 1 iteration
 % cfg = [];
@@ -34,8 +37,8 @@ cfg.location_idx = 400;
 rms_analysis_iter1_mult_paper(cfg);
 
 % Setup sample interval
-sample_idxs = 105:135; %250*0.428:250*0.532;
-for sample_idx=sample_idxs
+% sample_idxs = 105:135; %250*0.428:250*0.532;
+parfor sample_idx=105:135
     cfg = [];
     cfg.source_name = 'mult_cort_src_17';
     cfg.sample_idx = sample_idx;
@@ -53,8 +56,8 @@ cfg.location_idx = 400;
 rms_analysis_iter1_mult_paper(cfg);
 
 % Setup sample interval
-sample_idxs = 105:135; %250*0.428:250*0.532;
-for sample_idx=sample_idxs
+% sample_idxs = 105:135; %250*0.428:250*0.532;
+parfor sample_idx=105:135
     cfg = [];
     cfg.source_name = 'mult_cort_src_sine_2';
     cfg.sample_idx = sample_idx;
@@ -69,3 +72,5 @@ rms_analysis_iter1_summary(cfg);
 cfg = [];
 cfg.source_name = 'mult_cort_src_sine_2';
 rms_analysis_iter1_summary(cfg);
+
+aet_parallel_close(cfg_par);
