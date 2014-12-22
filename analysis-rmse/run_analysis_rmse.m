226f3668c3d73914e8054545301c91444a68eff6
pchrapka <pchrapka@gmail.com>
2014-12-22 11:36:09 -0500

    moving old rms analysis to a different folder

---
+++ analysis-rmse/run_analysis_rmse.m
@@ -0,0 +1,11 @@
+% Calculate rmse of data
+experiment_rmse
+
+% % Import power data to Brainstorm and plot power on surface
+% import_power
+
+% Save images with power plotted on cortical surface
+% save_power
+
+% Plot power surface
+% plot_power
