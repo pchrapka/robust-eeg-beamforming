4ce696400eab04a3a0833cb49e408e77744c5487
pchrapka <pchrapka@gmail.com>
2014-12-22 13:18:23 -0500

    added a few flags to rmse function for normalization and setting a custom alpha value

--- +metrics/rmse.m
+++ +metrics/rmse.m
@@ -13,7 +13,15 @@ function [output] = rmse(cfg)
 %   cfg.input
 %       [location/time components] where components can be a single
 %       component
+%   cfg.normalize
+%       flag to toggle least squares normalization of input and output,
+%       default = true;
+%   cfg.alpha
+%       (optional) custom normalization factor for bf_out, default =
+%       ignored
 %
+%   Output
+%   ------
 %   output.rmse_x
 %   output.rmse_y
 %   output.rmse_z
@@ -31,6 +39,9 @@ function [output] = rmse(cfg)
 %   output.rms_input_z
 %       power of the input for each component
 
+% Set defaults
+if ~isfield(cfg, 'normalize'), cfg.normalize = true; end
+
 
 if isvector(cfg.bf_out) && isvector(cfg.input)
     % Make sure they're both column vectors
@@ -41,29 +52,39 @@ else
     input = cfg.input;
 end
 
-% Calculate the power of the signals
-% The only reason for this step is so that we perform the least squares
-% fit over the power of the signal instead of the amplitude itself, to
-% avoid the normalization factor from flipping the sign
-bf_pow = sqrt(sum(bf_out.^2,2));
-input_pow = sqrt(sum(input.^2,2));
-
-% Calculate the support of the input
-select = input_pow > 0;
-if sum(select) == 0
-    % Display an error if the signal is 0
-    warning('metrics:rmse',...
-        ['the input signal is 0, cannot determine a reference '...
-        'for normalization']);
+% Check if we should normalize the beamformer output
+if cfg.normalize
+    % Calculate the power of the signals
+    % The only reason for this step is so that we perform the least squares
+    % fit over the power of the signal instead of the amplitude itself, to
+    % avoid the normalization factor from flipping the sign
+    bf_pow = sqrt(sum(bf_out.^2,2));
+    input_pow = sqrt(sum(input.^2,2));
+    
+    % % Calculate the support of the input
+    % select = input_pow > 0;
+    % if sum(select) == 0
+    %     % Display an error if the signal is 0
+    %     warning('metrics:rmse',...
+    %         ['the input signal is 0, cannot determine a reference '...
+    %         'for normalization']);
+    % end
+    
+    % Calculate the normalizing factor by least squares
+    % i.e. minimize the 2-norm between both signals
+    alpha = (input_pow'*bf_pow)...
+        /(bf_pow'*bf_pow);
+    
+    % Normalize the output power
+    bf_out_norm = bf_out*alpha;
+else
+    % Apply a custom normalization factor
+    if isfield(cfg, 'alpha')
+        bf_out_norm = bf_out*cfg.alpha;
+    else
+        bf_out_norm = bf_out;
+    end
 end
-
-% Calculate the normalizing factor by least squares
-% i.e. minimize the 2-norm between both signals
-alpha = (input_pow(select)'*bf_pow(select))...
-    /(bf_pow(select)'*bf_pow(select));
-
-% Normalize the output power
-bf_out_norm = bf_out*alpha;
 
 % Calculate the RMSE
 error_signal = bf_out_norm - input;
@@ -83,7 +104,7 @@ end
 if length(rmsedb) == 3
     output.rmsedb_x = rmsedb(1);
     output.rmsedb_y = rmsedb(1);
-    output.rmsedb_z = rmse(1);
+    output.rmsedb_z = rmsedb(1);
 else
     warning('metrics:rmse',...
         ['not sure how to output the rmsedb, size: '...
