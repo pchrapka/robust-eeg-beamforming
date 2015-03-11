c868b99f7edf4bf6255255a22714b82d5c1f24ba
pchrapka <pchrapka@gmail.com>
2015-03-11 16:09:07 -0400

    fixed up bash scripts

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-rsync -rvz --include='*.m' --exclude='.git' --exclude='output' ~/projects/robust-eeg-beamforming-paper/ chrapkpk@blade16:Documents/projects/robust-eeg-beamforming-paper/
+rsync -rvz --include='*.m' --exclude='.git' --exclude='output' --exclude='anatomy' ~/projects/robust-eeg-beamforming-paper/ chrapkpk@blade16:Documents/projects/robust-eeg-beamforming-paper/
