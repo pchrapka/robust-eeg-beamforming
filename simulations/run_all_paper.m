e259c7af87a01a4c1fc4efbf056bc9f51cbc69b5
pchrapka <pchrapka@gmail.com>
2015-06-25 10:58:29 -0400

    refactoring some code in sinr folder renamed analysis-metrics to analysis-sinr

--- simulations/run_all_paper.m
+++ simulations/run_all_paper.m
@@ -12,20 +12,35 @@ aet_parallel_init([]);
 % Running time: approx. 30 minutes for one analysis (one snr, matched and
 % mismatched scenarios and RMVB included)
 
-%% Paper simulations
-% PAPER
 % run_sim_vars_single_bem_paper
 % run_sim_vars_distr_bem_paper
 run_sim_vars_bem_paper_mult17
+
+
+% HD for beampatterns and power plots
+% run_sim_vars_bemhd_paper_mult17hd
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Analysis
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% SINR
-% Calculate SINR for different source configurations
-% PAPER
-metric_analysis_sinr
+% Calculate SINR for mult source configuration
+metric_analysis_sinr_mult_paper
+
+%% Power plots at t=t1
+
+% low res
+% TODO have data, redo script using beampattern as example
+% single
+% distr
+% mult
+
+% high res
+% TODO simulate data for single and distr
+% single
+% distr
+% mult
 
 %% Close the parallel pipeline
 aet_parallel_close([]);
