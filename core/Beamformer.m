17670b6260ed8d55b61cfd10644945ce6e9e958d
pchrapka <pchrapka@gmail.com>
2016-09-08 16:51:56 -0400

    added beamformer output method

--- core/Beamformer.m
+++ core/Beamformer.m
@@ -16,6 +16,33 @@ classdef Beamformer
     
     methods (Abstract)
         data = inverse(obj, H, R)
+        %INVERSE compute spatial filter
+        
+        data = beamformer_output(obj, W, signal)
+        %BEAMFORM_OUTPUT compute beamformer output
+    end
+    
+    methods
+        function data = output(obj, W, signal )
+            %OUTPUT compute beamformer output
+            %   OUTPUT(obj, W, signal) compute beamformer output
+            %
+            %   Input
+            %   -----
+            %   signal (matrix)
+            %       signal matrix [channels timepoints]
+            %   W (matrix)
+            %       spatial filter, [channels components], output of
+            %       inverse()
+            %
+            %   Output
+            %   ------
+            %   data            
+            %       dipole moment over time [components timepoints]
+            
+            data = W'*signal;
+            
+        end
     end
     
 end
