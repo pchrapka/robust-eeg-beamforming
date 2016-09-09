29f4307ba8a11e51875639928320a3b28b2525ad
Philip Chrapka <chrapkpk@blade16.ece.mcmaster.ca>
2016-09-09 11:42:45 -0400

    added some options to check snr function

--- core/check_eeg_snr.m
+++ core/check_eeg_snr.m
@@ -1,8 +1,9 @@
-function check_eeg_snr(data)
+function check_eeg_snr(data,varargin)
 
 p = inputParser();
 addRequired(p,'data',@(x) isstruct(x) || ischar(x));
-parse(p,data);
+addParameter(p,'ntrials',[],@(x) length(x) == 1);
+parse(p,data,varargin{:});
 
 if ischar(data)
     % data file name
@@ -13,6 +14,12 @@ if ischar(data)
     data = din.data;
 end
 
+if isempty(p.Results.ntrials)
+    ntrials = length(data.signal);
+else
+    ntrials = p.Results.ntrials;
+end
+
 % compute snr
 if isfield(data,'avg_signal')
     snr = aet_analysis_snr(data.avg_signal, data.avg_noise);
@@ -20,7 +27,7 @@ if isfield(data,'avg_signal')
 end
 
 if isfield(data,'signal')
-    for i=1:length(data.signal)
+    for i=1:ntrials
         snr = aet_analysis_snr(data.signal{i},data.noise{i});
         fprintf('snr: %0.2f db for trial %d\n',snr,i);
     end
