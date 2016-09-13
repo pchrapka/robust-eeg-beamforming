9eb36907e8bb1754e8b5506592ddceae6623734b
pchrapka <pchrapka@gmail.com>
2016-09-13 10:31:34 -0400

    fix mislabeled signal

--- source-configs/src_param_mult_cortical_source_17_lag40.m
+++ source-configs/src_param_mult_cortical_source_17_lag40.m
@@ -35,7 +35,7 @@ sim_cfg.sources{1}.moment = aet_sim_dipole_orientation(cfg)';
 %% Source 2
 % Source signal params for pr_peak()
 sim_cfg.sources{2} = sim_cfg.sources{1}; % Copy the first source
-sim_cfg.sources{2}.type = 'signal';
+sim_cfg.sources{2}.type = 'interference';
 sim_cfg.sources{2}.signal_type = 'erp';
 sim_cfg.sources{2}.snr = -10; % in dB
 sim_cfg.sources{2}.freq = 10;
