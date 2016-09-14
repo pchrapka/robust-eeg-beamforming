a10171d6ecb82a613519e01b0d5616f7a2613eda
Philip Chrapka <chrapkpk@blade16.ece.mcmaster.ca>
2016-09-14 12:41:52 -0400

    reorganized script

--- simulations/run_sinr.m
+++ simulations/run_sinr.m
@@ -1,10 +1,14 @@
 %% run_sinr
 
+
+%% data and beamformers
+
 run_sim_vars_bem_paper_mult17_lag40
-metric_analysis_sinr_mult('mult_cort_src_17_lag40');
-
 run_sim_vars_bem_paper_mult_sine
-metric_analysis_sinr_mult('mult_cort_src_sine_2');
-
 run_sim_vars_bem_paper_mult_sine_uncor
+
+%% sinr metrics
+
+metric_analysis_sinr_mult('mult_cort_src_sine_2');
+metric_analysis_sinr_mult('mult_cort_src_17_lag40');
 metric_analysis_sinr_mult('mult_cort_src_sine_2_uncor');
