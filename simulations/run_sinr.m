78d58aa3ea708c7a8e54aad411647be61a1af7fe
pchrapka <pchrapka@gmail.com>
2016-09-15 13:36:58 -0400

    added cov leadfield method

--- simulations/run_sinr.m
+++ simulations/run_sinr.m
@@ -6,11 +6,11 @@
 run_sim_vars_bem_paper_mult17_lag40
 run_sim_vars_bem_paper_mult_sine
 run_sim_vars_bem_paper_mult_sine_uncor
-run_sim_vars_bem_paper_covtrial_mult17
+% run_sim_vars_bem_paper_covtrial_mult17
 
 %% sinr metrics
 
 metric_analysis_sinr_mult('sim_data_bem_1_100t', 'mult_cort_src_sine_2');
 metric_analysis_sinr_mult('sim_data_bem_1_100t', 'mult_cort_src_17_lag40');
 metric_analysis_sinr_mult('sim_data_bem_1_100t', 'mult_cort_src_sine_2_uncor');
-metric_analysis_sinr_mult('sim_data_bem_1_1000t_noavg', 'mult_cort_src_17_randamp');
+% metric_analysis_sinr_mult('sim_data_bem_1_1000t_noavg', 'mult_cort_src_17_randamp');
