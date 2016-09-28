d2746de9c3866d58fc23d2e7a28ad0f105deb417
Phil Chrapka <pchrapka@gmail.com>
2016-09-28 16:02:44 -0400

    added check for pinv tolerance

--- core/check_pinv.m
+++ core/check_pinv.m
@@ -1,21 +1,24 @@
-function check_pinv(A,ncomps)
+function tol_new = check_pinv(A,ncomps)
 %
 %   A (matrix)
 %       matrix for pseudo inverse
 %   ncomps (integer)
 %       expected components
 
+
+tol_new = [];
 pinv_tol = max(size(A)) * norm(A) * eps(class(A));
-d = svd(pinv(A));
+d = svd(A);
 
 evalues = abs(d) >= pinv_tol;
 nevalues = sum(evalues);
 
 if ~isequal(nevalues,ncomps)
-    fprintf('number of eigenvalues: %d\nnumber of components: %d\n',nevalues,ncomps);
-    disp(evalues);
-    error(['number of evalues does not correspond with',...
-        'the expected number of components. consider adjusting pinv tolerance.']);
+    %warning('number of eigenvalues: %d\nnumber of components: %d\n',nevalues,ncomps);
+    %disp(evalues);
+    warning('adjusting pinv tolerance');
+    % calculate new tolerance
+    tol_new = ceil(d(ncomps+1)/pinv_tol)*pinv_tol;
 end
 
 end
