2ffbcb37d818353dae3c5d78591f1d10b50fc81c
pchrapka <pchrapka@gmail.com>
2016-10-30 19:58:06 -0400

    added lcmv eig filter to power plots altered beamformer analysis to take average after source localization

--- core/beamformer_analysis.m
+++ core/beamformer_analysis.m
@@ -202,7 +202,16 @@ n_scans = length(cfg.loc);
 scan_locs = cfg.loc;
 
 % select sample points based on the data_samples parameter
-data_trials = data.avg_trials(:,cfg.data_samples);
+switch cfg.cov_type
+    case 'time'
+        data_trials = data.avg_trials(:,cfg.data_samples);
+    case 'trial'
+        data_trials = cell(length(data.trials));
+        for i=1:length(data.trials)
+            temp_data = data.trials{i};
+            data_trials{i} = temp_data(:,cfg.data_samples);
+        end
+end
 
 % allocate mem
 beam_signal = zeros(n_scans, length(cfg.data_samples), n_components);
@@ -242,8 +251,18 @@ parfor i=1:n_scans
     out_filter{i} = beam_out.W;
     out_loc(i) = idx;
     
-    beam_signal(i,:,:) = beamformer.output(...
-        beam_out.W, data_trials, 'P', beam_out.P)';
+    switch cfg.cov_type
+        case 'time'
+            beam_signal(i,:,:) = beamformer.output(...
+                beam_out.W, data_trials, 'P', beam_out.P)';
+        case 'trial'
+            beam_signal_temp = zeros(length(data_trials),size(data_trials{1},1),size(data_trials{1},2));
+            for j=1:length(data_trials)
+                beam_signal_temp(j,:,:) = beamformer.output(...
+                    beam_out.W, data_trials{j}, 'P', beam_out.P)';
+            end
+            beam_signal(i,:,:) = squeeze(mean(beam_signal_temp,1));
+    end
     
 end
 fprintf('\n');
