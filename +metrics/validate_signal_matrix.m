199c93b560a9f17813a88ea25fc268e674b90c8e
Phil Chrapka <pchrapka@gmail.com>
2016-10-31 09:52:07 -0400

    remove a check

--- +metrics/validate_signal_matrix.m
+++ +metrics/validate_signal_matrix.m
@@ -2,9 +2,9 @@ function result = validate_signal_matrix(A)
 
 if ~ismatrix(A)
     result = false;
-elseif (size(A,1) > size(A,2))
-    % matrix should be channels x samples
-    error('samples should be > channels');
+% elseif (size(A,1) > size(A,2))
+%     % matrix should be channels x samples
+%     error('samples should be > channels');
 else
     result = true;
 end
