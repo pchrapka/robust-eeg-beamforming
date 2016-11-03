392be83f648ce51ff3abfb916a9d070589b0c99f
pchrapka <pchrapka@gmail.com>
2016-11-03 14:11:02 -0400

    added a new source config

--- source-configs/src_param_mult_cortical_source_17_lag40_randamp.m
+++ source-configs/src_param_mult_cortical_source_17_lag40_randamp.m
@@ -1,4 +1,4 @@
-%% src_param_mult_cortical_source_17_lag40
+%% src_param_mult_cortical_source_17_lag40_randamp
 % Multiple cortical sources
 % Different source locations, waveforms do not overlap
 % Dipoles are not both normal to the cortex
@@ -8,15 +8,16 @@
 
 % sim_cfg.force = true;
 
-sim_cfg.source_name = 'mult_cort_src_17_lag40';
+sim_cfg.source_name = 'mult_cort_src_17_lag40_randamp';
 
 
 %% Source 1
 % Source signal params for pr_peak()
 sim_cfg.sources{1}.type = 'signal';
-sim_cfg.sources{1}.signal_type = 'erp';
+sim_cfg.sources{1}.signal_type = 'erp-random-amplitude';
 sim_cfg.sources{1}.snr = -10; % in dB
 sim_cfg.sources{1}.amp = 2;
+sim_cfg.sources{1}.amp_var = 0.1;
 sim_cfg.sources{1}.freq = 10;
 sim_cfg.sources{1}.pos = 120;
 sim_cfg.sources{1}.jitter = 5;
@@ -36,7 +37,9 @@ sim_cfg.sources{1}.moment = aet_sim_dipole_orientation(cfg)';
 % Source signal params for pr_peak()
 sim_cfg.sources{2} = sim_cfg.sources{1}; % Copy the first source
 sim_cfg.sources{2}.type = 'interference';
-sim_cfg.sources{2}.signal_type = 'erp';
+sim_cfg.sources{2}.signal_type = 'erp-random-amplitude';
+sim_cfg.sources{2}.amp = 2;
+sim_cfg.sources{2}.amp_var = 0.1;
 sim_cfg.sources{2}.snr = -10; % in dB
 sim_cfg.sources{2}.freq = 10;
 sim_cfg.sources{2}.pos = 160;
