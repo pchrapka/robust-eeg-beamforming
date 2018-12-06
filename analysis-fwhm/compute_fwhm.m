8b9d67a9e87308838a07be2257164d7292a3ccb6
pchrapka <pchrapka@gmail.com>
2018-12-05 19:41:49 -0500

    added fwhm calc

---
+++ analysis-fwhm/compute_fwhm.m
@@ -0,0 +1,149 @@
+function [outputfiles] = compute_fwhm(...
+    data_set,beamformers, files_power, files_locerr, samples,source_idx,varargin)
+%compute_fwhm computes FWHM
+%   compute_fwhm computs FWHM
+%
+%   Input
+%   -----
+%   data_set (SimDataSetEEG object)
+%       original data set 
+%   beamformers (cell array)
+%       beamformer cfg file tags to process
+%   input_files (cell array)
+%       file names of power data for each beamformer
+%   samples (array)
+%       array of samples for power calculation
+%
+%   Parameters
+%   ----------
+%   source_idx 
+%       source voxel
+%   int_idx
+%       (optional) index of interfering source
+%   save (logical, default = true)
+%       flag for saving the plot
+%   force (logical, default = false)
+%       force recomputation
+%
+%   Output
+%   ------
+%   outputfiles
+%       cell array of output files
+%
+%   See also COMPUTE_POWER
+
+p = inputParser();
+addRequired(p,'data_set',@(x) isa(x,'SimDataSetEEG'));
+addRequired(p,'beamformers',@(x) ~isempty(x) && iscell(x));
+addRequired(p,'files_power',@(x) ~isempty(x) && iscell(x));
+addRequired(p,'files_locerr',@(x) ~isempty(x) && iscell(x));
+addRequired(p,'samples',@(x) ~isempty(x) && length(x) >= 1);
+addRequired(p,'source_idx',@(x) x > 1 && length(x) == 1);
+%addParameter(p,'int_idx',[],@(x) isempty(x) || (x > 1 && length(x) == 1));
+addParameter(p,'save',true,@islogical);
+addParameter(p,'force',false,@islogical);
+
+parse(p,data_set,beamformers,files_power,files_locerr,samples,source_idx,varargin{:});
+
+%% Options
+flag_debug = false;
+
+% Save options
+cfg_save = [];
+cfg_save.data_set = data_set;
+cfg_save.file_type = 'metrics';
+
+%% Localization error based on max power
+% allocate mem
+outputfiles = cell(length(beamformers),1);
+
+if length(p.Results.samples) > 1
+    idx_start = min(p.Results.samples);
+    idx_end = max(p.Results.samples);
+    tag_sample = sprintf('s%ds%d',...
+        idx_start, idx_end);
+else
+    tag_sample = sprintf('s%d',...
+        p.Results.samples);
+end
+
+for i=1:length(beamformers)
+    
+    cfg_save.file_tag = sprintf('%s_fwhm_%s', beamformers{i}, tag_sample);
+    outputfiles{i} = metrics.filename(cfg_save);
+    
+     % Skip the computation if the file exists
+    if exist(outputfiles{i}, 'file') && ~p.Results.force
+        print_msg_filename(outputfiles{i},'Skipping');
+        fprintf('\tAlready exists\n');
+        continue;
+    else
+        print_msg_filename(outputfiles{i},'Working on');
+    end
+    
+    % Load data 
+    din = load(files_power{i});
+    power_data = din.data.power(:,p.Results.samples);
+    if size(power_data,2) > 1
+        power_data = mean(power_data,2);
+    end
+    
+    % Load loc error
+    din2 = load(files_locerr{i});
+    power_max = din2.power_max;
+    threshold_fwhm = power_max/2;
+    idx_bool_fwhm = power_data > threshold_fwhm;
+    idx_fwhm = 1:size(power_data,1);
+    idx_fwhm = idx_fwhm(idx_bool_fwhm);
+    
+    % Load head model
+    dinbf = load(din.data.bf_file);
+    
+    % get head model config
+    if isfield(dinbf.source.head_cfg,'actual')
+        head_cfg = dinbf.source.head_cfg.actual;
+    else
+        head_cfg = dinbf.source.head_cfg;
+    end
+    
+    % load head model
+    hmfactory = HeadModel();
+    hm = hmfactory.createHeadModel(head_cfg.type,head_cfg.file);
+    hm.load();
+    
+    [~,loc_max] = hm.get_vertices('type', 'index', 'idx', din2.data.localization_index);
+    
+    threshold_outlier = 4/100;
+    distances = zeros(length(idx_fwhm),1);
+    for j=1:length(idx_fhwm)
+        [~,loc] = hm.get_vertices('type', 'index', 'idx', idx_fhwm(j));
+        distances(j) = pdist([loc_max; loc], 'euclidean');
+    end
+    
+    if flag_debug
+        h = figure();
+        hist(distances);
+        close(h);
+    end
+    
+    % filter out outliers
+    distances(distances > threshold_outlier) = 0;
+    
+    % get max distance from center
+    [max_dist,~] = max(distances);
+    
+    data = [];
+    data.fwhm = threshold_fwhm;
+    data.fwhm_radius = max_dist;
+    % Copy data
+    data.name = beamformers{i};
+    data.bf_file = din.data.bf_file;
+    data.data_set = data_set;
+    
+    % Save output data
+    print_save(outputfiles{i});
+    save(outputfiles{i}, 'data');
+    save(strrep(outputfiles{i},'.mat','.txt'), 'fwhm_radius', '-ascii');
+end
+
+end
---
+++ analysis-fwhm/compute_fwhm_bemhd_config_paper.m
@@ -0,0 +1,94 @@
+function compute_fwhm_bemhd_config_paper(sim_file,source_name,varargin)
+
+p = inputParser();
+addRequired(p,'sim_file',@ischar);
+addRequired(p,'source_name',@ischar);
+addParameter(p,'PlotGroups',{'matched-power','mismatched-power'},@iscell);
+addParameter(p,'datatag','locsall_covtime',@ischar);
+addParameter(p,'snr',0,@isnumeric);
+parse(p,sim_file,source_name,varargin{:});
+
+%% Compute power
+force = false;
+params = [];
+
+for i=1:length(p.Results.PlotGroups)
+    if isempty(params)
+        params = get_plot_group(p.Results.PlotGroups{i}, p.Results.datatag);
+    else
+        params(i) = get_plot_group(p.Results.PlotGroups{i}, p.Results.datatag);
+    end
+end
+
+%% Set vars
+eval(sim_file);
+samples_avg{1} = 1:sim_cfg.timepoints;
+clear sim_cfg;
+
+% TODO move samples out and as a parameter
+
+% switch p.Results.config
+%     case 'mult-paper'
+        source_idx = 5440;
+%         source_args = {'source_idx',5440,'int_idx',13841};
+        samples = [120,160];
+        samples_avg{2} = 110:170;
+%     case 'single-paper'
+%         source_args = {'source_idx',5440};
+%         samples = 120;
+%         samples_avg{2} = 110:130;
+%     otherwise
+%         error('unknown config');
+% end
+
+%% average power
+for j=1:length(samples_avg)
+    sample = samples_avg{j};
+    for i=1:length(params)
+        data_set = SimDataSetEEG(...
+            sim_file,...
+            source_name,...
+            p.Results.snr,...
+            'iter',1);
+        
+        % Compute the beamformer output power
+        files_power = compute_power(...
+            data_set, params(i).beamformer_configs,'force', force);
+        
+        % Compute localization error
+        files_locerr = compute_localization_error(...
+            data_set, params(i).beamformer_configs, files_power,...
+            sample, source_idx, 'force', force);
+        
+        compute_fwhm(...
+            data_set, params(i).beamformer_configs, files_power, files_locerr,...
+            sample, source_idx, 'force', force);
+    end
+end
+
+%% instantaneous power
+for j=1:length(samples)
+    sample = samples(j);
+    for i=1:length(params)
+        data_set = SimDataSetEEG(...
+            sim_file,...
+            source_name,...
+            p.Results.snr,...
+            'iter',1);
+        
+        % Compute the beamformer output power
+        files_power = compute_power(...
+            data_set, params(i).beamformer_configs,'force', force);
+        
+        % Compute localization error
+        files_locerr = compute_localization_error(...
+            data_set, params(i).beamformer_configs, files_power,...
+            sample, source_idx, 'force', force);
+        
+        compute_fwhm(...
+            data_set, params(i).beamformer_configs, files_power, files_locerr,...
+            sample, source_idx, 'force', force);
+    end
+end
+
+end
