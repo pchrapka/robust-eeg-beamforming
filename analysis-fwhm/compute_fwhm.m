13bd8266e9acb4fcf01b80794d07453d6451beab
Phil <chrapkpk@mcmaster.ca>
2018-12-09 09:35:07 -0500

    added fhwm based in max-min range instead of just max-0 range

--- analysis-fwhm/compute_fwhm.m
+++ analysis-fwhm/compute_fwhm.m
@@ -57,7 +57,8 @@ cfg_save.file_type = 'metrics';
 %% Localization error based on max power
 % allocate mem
 outputfiles = cell(length(beamformers),1);
-fwhm_radius = zeros(length(beamformers),1);
+fwhm_radius_all = zeros(length(beamformers),1);
+fwhm_radius2_all = zeros(length(beamformers),1);
 
 if length(p.Results.samples) > 1
     idx_start = min(p.Results.samples);
@@ -79,7 +80,8 @@ for i=1:length(beamformers)
         print_msg_filename(outputfiles{i},'Skipping');
         fprintf('\tAlready exists\n');
         din = load(outputfiles{i});
-        fwhm_radius(i) = din.data.fwhm_radius;
+        fwhm_radius_all(i) = din.data.fwhm_radius;
+        fwhm_radius2_all(i) = din.data.fwhmmaxmin_radius;
         continue;
     else
         print_msg_filename(outputfiles{i},'Working on');
@@ -92,25 +94,8 @@ for i=1:length(beamformers)
         power_data = mean(power_data,2);
     end
     
-    % Load loc error
-    din2 = load(files_locerr{i});
-    power_max = din2.data.power_max;
-    threshold_fwhm = power_max/2;
-    idx_bool_fwhm = power_data > threshold_fwhm;
-    idx_fwhm = 1:size(power_data,1);
-    idx_fwhm = idx_fwhm(idx_bool_fwhm);
-    
-    if flag_debug
-        power_data_sorted = sort(power_data,1,'descend');
-        figure();
-        plot(power_data_sorted,'b');
-        hold on;
-        plot(threshold_fwhm*ones(size(power_data_sorted)),'r');
-    end
-    
     % Load head model
     dinbf = load(din.data.bf_file);
-    
     % get head model config
     if isfield(dinbf.source.head_cfg,'actual')
         head_cfg = dinbf.source.head_cfg.actual;
@@ -118,52 +103,22 @@ for i=1:length(beamformers)
         head_cfg = dinbf.source.head_cfg;
     end
     
-    % load head model
-    hmfactory = HeadModel();
-    hm = hmfactory.createHeadModel(head_cfg.type,head_cfg.file);
-    hm.load();
+    % Load loc error
+    din2 = load(files_locerr{i});
+    power_max = din2.data.power_max;
     
-    [~,loc_max] = hm.get_vertices('type', 'index', 'idx', din2.data.localization_index);
+    [fwhm_threshold, fwhm_radius] = fwhm_private(power_data, head_cfg,...
+        power_max, din2.data.localization_index);
+    [fwhm_threshold2, fwhm_radius2] = fwhm_private(power_data, head_cfg,...
+        power_max, din2.data.localization_index, 'power_min', min(power_data));
     
-    distances = zeros(length(idx_fwhm),1);
-    for j=1:length(idx_fwhm)
-        [~,loc] = hm.get_vertices('type', 'index', 'idx', idx_fwhm(j));
-        distances(j) = pdist([loc_max; loc], 'euclidean');
-    end
-    
-    % sort distances
-    [dist_sorted, idx_sorted] = sort(distances,1,'ascend');
-    dist_diff = diff(dist_sorted);
-    % check if there's a dstance break in powers that passed the fwhm/2
-    % threshold
-    idx_outlier = find(dist_diff > 0.01);
-    
-    if flag_debug
-        h = figure();
-        hist(distances);
-        close(h);
-        
-        h = figure();
-        A = [distances(:) power_data(idx_fwhm)];
-        A = sortrows(A,1);
-        plot(A(:,1), A(:,2));
-        hold on;
-        plot(distances(idx_sorted(idx_outlier))*ones(size(A,1),1),...
-            linspace(min(A(:,2)), max(A(:,2)), size(A,1)), 'r');
-        xlabel('distance');
-        ylabel('power');
-        close(h);
-    end
-    
-    distances = distances(idx_sorted(1:idx_outlier));
-    
-    % get max distance from center
-    [max_dist,~] = max(distances);
-    
-    fwhm_radius(i) = max_dist;
+    fwhm_radius_all(i) = fwhm_radius;
+    fwhm_radius2_all(i) = fwhm_radius2;
     data = [];
-    data.fwhm = threshold_fwhm;
-    data.fwhm_radius = max_dist;
+    data.fwhm = fwhm_threshold;
+    data.fwhm_radius = fwhm_radius;
+    data.fwhmmaxmin = fwhm_threshold2;
+    data.fwhmmaxmin_radius = fwhm_radius2;
     % Copy data
     data.name = beamformers{i};
     data.bf_file = din.data.bf_file;
@@ -192,10 +147,86 @@ else
 end
 
 fid = fopen(outputfile_all, 'w');
-fprintf(fid, 'Beamformer,FWHM radius\n');
+fprintf(fid, 'Beamformer,FWHM radius,FWHM radius (max-min)\n');
 for i=1:length(beamformers)
-    fprintf(fid, '%s,%0.6g\n', beamformers{i}, fwhm_radius(i));
+    fprintf(fid, '%s,%0.6g,%0.6g\n', beamformers{i}, fwhm_radius_all(i), fwhm_radius2_all(i));
 end
 fclose(fid);
 
+end
+
+function [fwhm_threshold, fwhm_radius] = fwhm_private(...
+    power_data, head_cfg, power_max, power_max_index, varargin)
+p = inputParser();
+addRequired(p,'power_data',@(x) ~isempty(x) && length(x) >= 1);
+addRequired(p,'head_cfg', @isstruct);
+addRequired(p,'power_max',@(x) isnumeric(x) && length(x) == 1);
+addRequired(p,'power_max_index',@(x) isnumeric(x) && length(x) == 1);
+addParameter(p,'power_min',[],@(x) isnumeric(x) && length(x) == 1);
+
+parse(p,power_data, head_cfg, power_max, power_max_index, varargin{:});
+
+flag_debug = false;
+
+if isempty(p.Results.power_min)
+    fwhm_threshold = power_max/2;
+else
+    fwhm_threshold = (power_max - p.Results.power_min)/2;
+end
+idx_bool_fwhm = power_data > fwhm_threshold;
+idx_fwhm = 1:size(power_data,1);
+idx_fwhm = idx_fwhm(idx_bool_fwhm);
+
+if flag_debug
+    power_data_sorted = sort(power_data,1,'descend');
+    figure();
+    plot(power_data_sorted,'b');
+    hold on;
+    plot(fwhm_threshold*ones(size(power_data_sorted)),'r');
+end
+
+% load head model
+hmfactory = HeadModel();
+hm = hmfactory.createHeadModel(head_cfg.type,head_cfg.file);
+hm.load();
+
+[~,loc_max] = hm.get_vertices('type', 'index', 'idx', power_max_index);
+
+distances = zeros(length(idx_fwhm),1);
+for j=1:length(idx_fwhm)
+    [~,loc] = hm.get_vertices('type', 'index', 'idx', idx_fwhm(j));
+    distances(j) = pdist([loc_max; loc], 'euclidean');
+end
+
+% sort distances
+[dist_sorted, idx_sorted] = sort(distances,1,'ascend');
+dist_diff = diff(dist_sorted);
+% check if there's a dstance break in powers that passed the fwhm/2
+% threshold
+idx_outlier = find(dist_diff > 0.01);
+if isempty(idx_outlier)
+    idx_outlier = length(idx_sorted);
+end
+
+if flag_debug
+    h = figure();
+    hist(distances);
+    close(h);
+    
+    h = figure();
+    A = [distances(:) power_data(idx_fwhm)];
+    A = sortrows(A,1);
+    plot(A(:,1), A(:,2));
+    hold on;
+    plot(distances(idx_sorted(idx_outlier))*ones(size(A,1),1),...
+        linspace(min(A(:,2)), max(A(:,2)), size(A,1)), 'r');
+    xlabel('distance');
+    ylabel('power');
+    close(h);
+end
+
+distances_thresh = distances(idx_sorted(1:idx_outlier));
+
+% get max distance from center
+[fwhm_radius,~] = max(distances_thresh);
 end
